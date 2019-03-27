53c7d3995f38bbd6d1075094b30fa31cc7bef024
Abir <abirhammache@gmail.com>
2019-03-27 02:55:00 -0700

    feat: the ui of a new announcement form

--- app/src/main/java/com/autolink/sayaradz/ui/fragment/oldcar/AnnouncementsFragment.kt
+++ app/src/main/java/com/autolink/sayaradz/ui/fragment/oldcar/AnnouncementsFragment.kt
@@ -4,17 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.autolink.sayaradz.R
+import com.autolink.sayaradz.databinding.FragmentAnnouncementsBinding
 
 
 class AnnouncementsFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        inflater.inflate(R.layout.fragment_announcements,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val binding: FragmentAnnouncementsBinding = DataBindingUtil.inflate(
+            inflater,R.layout.fragment_announcements,container,false
+        )
+
+        binding.announcementFab.setOnClickListener(
+
+            Navigation.createNavigateOnClickListener(
+                R.id.action_announcementsFragment_to_newAnnouncementFragment
+            )
+
+        )
+
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/autolink/sayaradz/ui/fragment/oldcar/NewAnnouncementFragment.kt
@@ -0,0 +1,74 @@
+package com.autolink.sayaradz.ui.fragment.oldcar
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+
+
+import com.autolink.sayaradz.R
+import com.autolink.sayaradz.databinding.FragmentNewAnnouncementBinding
+import android.content.DialogInterface
+import android.app.AlertDialog
+
+
+class NewAnnouncementFragment : Fragment() {
+
+    lateinit var binding: FragmentNewAnnouncementBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,R.layout.fragment_new_announcement,container,false
+        )
+
+        binding.carImage.setOnClickListener{
+            uploadPhoto()
+        }
+
+        binding.confirmBtn.setOnClickListener{
+            confirmDialog()
+        }
+
+
+        return binding.root
+    }
+
+    private fun uploadPhoto() {
+
+        startActivityForResult(getUploadIntent(),1000)
+    }
+
+    private fun getUploadIntent(): Intent? {
+        val shareIntent=Intent(Intent.ACTION_GET_CONTENT)
+        shareIntent.setType("image/*")
+        return shareIntent
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            binding.carImage.setImageURI(data?.data)
+    }
+
+    private fun confirmDialog() {
+        val builder = AlertDialog.Builder(context,R.style.AlertDialogCustom)
+        builder
+            .setMessage("Voulez vous confirmer l'annonce que vous venez de creéer ? ")
+            .setPositiveButton("Oui", DialogInterface.OnClickListener { dialog, id ->
+                // Yes-code
+            })
+            .setNegativeButton("Non", DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+            .show()
+    }
+
+}
